69e9377e3f9d54bfbc468bc88084a6945f103fc8
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-18 09:52:09 +0530

    Strapi Need to set up

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,19 +1,48 @@
 import Image from "next/image";
-import { LayoutGrid, Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   return (
-    <div>
+    <div className="shadow-md p-5 flex w-full justify-between">
       <div className="flex-1 flex items-center gap-8 mx-auto mt-5">
         <Image src="/logo.png" alt="Logo" width={150} height={100} />
 
-        <h2 className="flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200">
-          <LayoutGrid className="h-5 w-5 " /> Category
-        </h2>
-        <div className="flex gap-3 item-center border rounded-2xl p-2 px-5 ">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <h2 className=" cursor-pointer md:flex hidden gap-2 items-center border rounded-full p-2 px-10 bg-slate-100">
+              <LayoutGrid className="h-5 w-5" /> Category
+            </h2>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Browse Catergory</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <div className="md:flex gap-3 item-center border rounded-2xl p-2 px-5 hidden">
           <Search />
           <input type="text" placeholder="Search" className="outline-none" />
         </div>
+      </div>
+      <div className="flex gap-5 items-center">
+        <h2 className="flex gap-2 items-center text-lg">
+          <ShoppingBag /> 0
+        </h2>
+
+        <Button>Login</Button>
       </div>
     </div>
   );
