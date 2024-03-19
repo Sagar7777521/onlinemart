b6d45c0431c287ae85123a0a841c763eb2bfba23
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-20 01:48:30 +0530

    Remote Patterns

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
@@ -9,12 +10,33 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+import GlobalApi from "../utils/GlobalApi";
 
 const Header = () => {
+  const [categoryList, setCategoryList] = useState([]);
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((resp) => {
+      console.log(resp.data.data);
+      setCategoryList(resp.data.data);
+    });
+  };
   return (
     <div className="shadow-md p-5 flex w-full justify-between">
       <div className="flex-1 flex items-center gap-8 mx-auto mt-5">
-        <Image src="/logo.png" alt="Logo" width={150} height={100} />
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={150}
+          height={100}
+          quality={75}
+          priority={false}
+        />
 
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -25,10 +47,21 @@ const Header = () => {
           <DropdownMenuContent>
             <DropdownMenuLabel>Categories</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Team</DropdownMenuItem>
-            <DropdownMenuItem>Subscription</DropdownMenuItem>
+            {categoryList.map((category, index) => (
+              <DropdownMenuItem>
+                <Image
+                  src={
+                    process.env.NEXT_PUBLIC_BACKEND_BASE_URL +
+                    category?.attributes?.icon?.attributes?.url
+                  }
+                  unoptimized={true}
+                  alt="icon"
+                  width={23}
+                  height={23}
+                />
+                <h2>{category?.attributes?.name}</h2>
+              </DropdownMenuItem>
+            ))}
           </DropdownMenuContent>
         </DropdownMenu>
 
